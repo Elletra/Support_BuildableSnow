dd13ee861e98810258640074f53ddabb17a2233d
Electrk <Electrk@ymail.com>
2020-01-07 05:37:35 -0800

    Added explanation of how the add-on works.

--- createBrick.cs
+++ createBrick.cs
@@ -35,14 +35,39 @@ function BuildableSnow_CreateSnowBrick ( %gridX, %gridY, %gridZ )
 
 	BuildableSnowBrickset.add (%brick);
 
+	// Basically how this add-on works is that it maintains a grid of vertices and a grid of bricks.
+	// Four vertices make up a "tile".  The "tiles" in this case are bricks.
+	//
+	// Each brick has four vertices:
+	//   1. Top left corner of the brick.
+	//   2. Top right corner of the brick.
+	//   3. Bottom left corner of the brick.
+	//   4. Bottom right corner of the brick.
+	//
+	// Each vertex has only two heights: 0 or 1.
+	// Each snow brick datablock corresponds to a certain configuration of four vertex heights.
+	//
+	// Any time a brick is updated, it checks its four vertex heights and attempts to set its
+	// datablock to the appropriate one.  If it successfully changes its datablock, it updates
+	// its immediate neighbors.
+	//
+	// See fxDTSBrick::setSnowVertices() and fxDTSBrick::updateSnow() to see how this works.
+	//
+
+	//* The brick's position in the "tile" grid. *//
+
 	%brick.snowGridX = %gridX;
 	%brick.snowGridY = %gridY;
 	%brick.snowGridZ = %gridZ;
+
+	//* The brick's vertex coordinates: two X values and two Y values. *//
 
 	%brick.snowVertexLeft   = %gridX;      // Leftmost X vertex coordinate
 	%brick.snowVertexRight  = %gridX + 1;  // Rightmost X vertex coordinate
 	%brick.snowVertexTop    = %gridY;      // Topmost Y vertex coordinate
 	%brick.snowVertexBottom = %gridY + 1;  // Bottommost Y vertex coordinate
+
+	//* If there's already a brick at this position, delete it. *//
 
 	%existingBrick = $BuildableSnow::Grid::Brick_[%gridX, %gridY, %gridZ];
 
