84bde4b6bdbc2a41844b43177204118890a94d63
Electrk <Electrk@ymail.com>
2020-01-07 08:01:28 -0800

    Added comments/explanation to snowAdapterCheck()

--- updateSnow.cs
+++ updateSnow.cs
@@ -135,37 +135,45 @@ function fxDTSBrick::snowAdapterCheck ( %this, %data )
 	{
 		// Top left adapter
 		case $BuildableSnow::DataBlock_[0, 1, 1, 1]:
+
 			// The two directions we're going to check.
 			%coordX = -1;  // Which neighbor to check in the X direction.
 			%coordY = -1;  // Which neighbor to check in the Y direction.
 
-			// Vertices that need to be set to zero for this brick to be able to change.
-			%emptyVert0 = 1;  // Corresponds to %coordX
-			%emptyVert1 = 2;  // Corresponds to %coordY
+			// Vertices that need to be set to 0 for this brick to be able to change.
+			// Values are indices of the vertex order: topLeft, topRight, bottomLeft, bottomRight.
+			//
+			// So for this one it would be topRight (1) and bottomLeft (2).
+			//
+			%emptyVert0 = 1;  // Used with %coordX
+			%emptyVert1 = 2;  // Used with %coordY
 
 		// Top right adapter
 		case $BuildableSnow::DataBlock_[1, 0, 1, 1]:
+
 			%coordX =  1;
 			%coordY = -1;
 
-			%emptyVert0 = 0;
-			%emptyVert1 = 3;
+			%emptyVert0 = 0;  // topLeft (0)
+			%emptyVert1 = 3;  // bottomRight (3)
 
 		// Bottom left adapter
 		case $BuildableSnow::DataBlock_[1, 1, 0, 1]:
+
 			%coordX = -1;
 			%coordY =  1;
 
-			%emptyVert0 = 3;
-			%emptyVert1 = 0;
+			%emptyVert0 = 3;  // bottomRight (3)
+			%emptyVert1 = 0;  // topLeft (0)
 
 		// Bottom right adapter
 		case $BuildableSnow::DataBlock_[1, 1, 1, 0]:
+
 			%coordX = 1;
 			%coordY = 1;
 
-			%emptyVert0 = 2;
-			%emptyVert1 = 1;
+			%emptyVert0 = 2;  // bottomLeft (2)
+			%emptyVert1 = 1;  // topRight (1)
 
 		default:
 			return false;
