4442fb4046140328e8e784647694e948acec821b
Electrk <Electrk@ymail.com>
2020-01-07 06:02:43 -0800

    Minor code cleanup and comment edits

--- snowVertices.cs
+++ snowVertices.cs
@@ -2,10 +2,10 @@
 //
 // This does not change the brick's datablock (@see fxDTSBrick::updateSnow).
 //
-// @param {boolean} topLeft
-// @param {boolean} topRight
-// @param {boolean} bottomLeft
-// @param {boolean} bottomRight
+// @param {boolean} topLeft     - Height of the top left vertex (0 or 1).
+// @param {boolean} topRight    - Height of the top right vertex (0 or 1).
+// @param {boolean} bottomLeft  - Height of the bottom left vertex (0 or 1).
+// @param {boolean} bottomRight - Height of the bottom right vertex (0 or 1).
 //
 // @returns {boolean} Whether or not the operation was successful.  Use $BuildableSnow::LastError
 //                    to check for errors.
@@ -23,8 +23,6 @@ function fxDTSBrick::setSnowVertices ( %this, %topLeft, %topRight, %bottomLeft, 
 	%top    = %this.snowVertexTop;
 	%bottom = %this.snowVertexBottom;
 
-	%x = %this.snowGridX;
-	%y = %this.snowGridY;
 	%z = %this.snowGridZ;
 
 	$BuildableSnow::Grid::Vertex_[%left,  %top,    %z] = %topLeft;
@@ -39,10 +37,10 @@ function fxDTSBrick::setSnowVertices ( %this, %topLeft, %topRight, %bottomLeft, 
 
 // Gets the actual snow vertex data.
 //
-// Since the brick's datablock does not always correspond with the actual vertex data
-// for aesthetic purposes, we want to be able to accurately get the vertex data.
+// Since the brick's datablock does not always correspond with the actual vertex data for aesthetic
+// purposes, we want to be able to accurately get the vertex data.
 //
-// @returns {BuildableSnowVertices|null} A string with the vertices separated by a space in this order:
+// @returns {BuildableSnowVertices|null} A string with vertices separated by a space in this order:
 //                                       top left, top right, bottom left, bottom right.
 //                                       Or an empty string (null) if it's not a snow brick.
 //
