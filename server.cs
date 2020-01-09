0139e414879470d9e93c1ced40970f50835c0e12
Electrk <Electrk@ymail.com>
2020-01-08 19:02:57 -0800

    Added comments explaining what the files are

--- server.cs
+++ server.cs
@@ -5,13 +5,24 @@ if ( !isFunction ("getClosestPaintColor") )
 
 exec ("./lib/Support_CreateBrick.cs");
 
+// ------------------------------------------------
+
+
+// Default snow bricks.
 exec ("./bricks/datablocks.cs");
 
+// Constant values.
 exec ("./constants.cs");
+
+// Configurable variables that can be changed by mods.
 exec ("./config.cs");
+
+// Main code.
+exec ("./grid/exec.cs");
 exec ("./createBrick.cs");
 exec ("./snowVertices.cs");
 exec ("./updateSnow.cs");
 exec ("./raiseLower.cs");
-exec ("./grid/exec.cs");
+
+// Functions that only work in debug mode.
 exec ("./debug.cs");
