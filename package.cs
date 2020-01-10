6940057d80ccc5bdf591bc213223ca32cd9af187
Electrk <Electrk@ymail.com>
2020-01-10 06:36:46 -0800

    Added serverCmdPlantBrick blocking if it's a snow brick

--- package.cs
+++ package.cs
@@ -1,12 +1,27 @@
 package Support_BuildableSnow
 {
-	// Prevent tricky players from trying to plant snow bricks.
 	function serverCmdInstantUseBrick ( %client, %data )
 	{
 		if ( isObject (%data)  &&  !%data.isSnowBrick )
 		{
 			Parent::serverCmdInstantUseBrick (%client, %data);
 		}
+	}
+
+	function serverCmdPlantBrick ( %client )
+	{
+		if ( isObject (%player = %client.player)  &&  isObject (%tempBrick = %player.tempBrick) )
+		{
+			if ( %tempBrick.dataBlock.isSnowBrick )
+			{
+				%tempBrick.delete ();
+				%player.tempBrick = "";
+
+				return;
+			}
+		}
+
+		Parent::serverCmdPlantBrick (%client);
 	}
 };
 activatePackage (Support_BuildableSnow);
